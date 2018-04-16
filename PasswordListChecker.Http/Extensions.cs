5a7c4e9c24c7d9d3ae4bf4965db3387273bdf55a
Michael Coxon <michaelcoxon@outlook.com.au>
2018-04-16 23:54:24 +0800

    added extensions

---
+++ PasswordListChecker.Http/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordListChecker
+{
+    public static class Extensions
+    {
+        public static PasswordListBuilder AddUriSource(this PasswordListBuilder builder, Uri uri, IPasswordListDeserializer passwordListDeserializer = null)
+        {
+            passwordListDeserializer = passwordListDeserializer ?? new NewLineSeparatedPasswordListDeserializer();
+
+            var source = new HttpPasswordListSource(uri, passwordListDeserializer);
+            builder.AddSource(source);
+
+            return builder;
+        }
+
+        public static PasswordListBuilder AddUriSource(this PasswordListBuilder builder, string uri, IPasswordListDeserializer passwordListDeserializer = null)
+        {
+            passwordListDeserializer = passwordListDeserializer ?? new NewLineSeparatedPasswordListDeserializer();
+
+            var source = new HttpPasswordListSource(uri, passwordListDeserializer);
+            builder.AddSource(source);
+
+            return builder;
+        }
+    }
+}
